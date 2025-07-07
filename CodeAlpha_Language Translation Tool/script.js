e4f1ca6fe1a11771895d72c5ef6aafe2f8d1df56
JemanusAlan <jkjemanusalan@gmail.com>
2025-07-07 20:15:32 +0530

    Add files via upload

---
+++ CodeAlpha_Language Translation Tool/script.js
@@ -0,0 +1,30 @@
+async function translateText() {
+  const inputText = document.getElementById("inputText").value;
+  const sourceLang = document.getElementById("sourceLang").value;
+  const targetLang = document.getElementById("targetLang").value;
+
+  const url = "https://google-translate113.p.rapidapi.com/api/v1/translator/text";
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-rapidapi-host": "google-translate113.p.rapidapi.com",
+      "x-rapidapi-key": "5b17d3499fmsh64fdd5f73a9ddecp11fb85jsnc18ac85e1682"
+    },
+    body: JSON.stringify({
+      from: sourceLang,
+      to: targetLang,
+      text: inputText
+    })
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    document.getElementById("translatedText").innerText = result.trans;
+  } catch (error) {
+    console.error("Translation failed:", error);
+    document.getElementById("translatedText").innerText = "Translation failed!";
+  }
+}
